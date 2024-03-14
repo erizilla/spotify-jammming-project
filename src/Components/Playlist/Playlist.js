0f30a6955af837ac2691a442f146cf060094c43e
erizilla <39068554+erizilla@users.noreply.github.com>
2024-03-13 23:05:36 -0700

    changed

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -17,7 +17,7 @@ import TrackList from "../TrackList/TrackList";
         return (
             <div className="Playlist">
                 <input defaultValue={"New Playlist"} onChange={this.handleNameChange} />
-                {/* <!-- Add a TrackList component --> */}
+                
                 <TrackList tracks={this.props.playlistTracks}
                             onRemove={this.props.onRemove}
                             isRemoval={true} />
